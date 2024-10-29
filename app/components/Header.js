952e72a9c4d1507d9d464fa1d37cc422ab42bc44
jordan-frampton <jordan.frampton@flybreeze.com>
2024-10-28 20:58:07 -0600

    more menu animations and states

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,20 +3,38 @@
 import Image from "next/image";
 import PropTypes from "prop-types";
 import "@/styles/header.css";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import Link from "next/link";
 
 export function Header({color}) {
     const [showMenu, setShowMenu] = useState(false);
+
+    const [windowWidth, setWindowWidth] = useState(1000);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        // Cleanup event listener on component unmount
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     const handleMenuClick = () => {
         const newMenuState = !showMenu;
         setShowMenu(newMenuState);
         if (newMenuState) {
+            document.body.classList.add("no-scroll");
             document.getElementById("menu-background").classList.remove("reverse");
             document.getElementById("menu-background").classList.add("animate");
             document.getElementById("menu").classList.remove("hide");
             document.getElementById("menu").classList.add("show");
         } else {
+            document.body.classList.remove("no-scroll");
             document.getElementById("menu-background").classList.remove("animate");
             document.getElementById("menu-background").classList.add("reverse");
             document.getElementById("menu").classList.remove("show");
@@ -27,9 +45,10 @@ export function Header({color}) {
     return (
         <>
             <div className={"sticky top-0 z-10"}>
-                <div className={"absolute flex justify-center w-full py-5"}>
-                    <p className={`ml-auto ${color === 'black' ? 'text-black' : 'text-[#EFEEE8]'}`}>Black
-                        Ink</p>
+                <div className={"absolute top-[40px] flex w-full z-20"}>
+                    <p className={`${(color === 'black' && !showMenu) || (showMenu && windowWidth < 768) ? 'text-black' : 'text-[#EFEEE8]'} absolute left-[40px] md:left-[50%] md:-translate-x-1/2 transition-all duration-1000 ease-in-out`}>
+                        BLACK INK
+                    </p>
                     <Image
                         id="logo"
                         src={`${color === 'black' || showMenu ? '/images/black-ink-logo-black.png' : '/images/black-ink-logo-cream.png'}`}
@@ -37,20 +56,33 @@ export function Header({color}) {
                         width={50}
                         height={50}
                         priority={true}
-                        className={`h-[21px] w-fit ml-auto mr-5 cursor-pointer z-20`}
+                        className={`absolute right-[40px] h-[21px] w-fit cursor-pointer`}
                         onClick={handleMenuClick}
                     />
                 </div>
                 <div id="menu-background" className={`circle`}
                      onClick={handleMenuClick}></div>
                 <div id="menu"
-                     className={`z-20 menu mt-10 ml-10 w-1/3 text-[36px] font-ritma`}
+                     className={`flex z-20 items-center md:items-start w-screen h-screen menu text-[36px] font-ritma`}
+                     onClick={handleMenuClick}
                 >
-                    <p className={"border-t border-t-black py-6 cursor-pointer"}>Home</p>
-                    <p className={"border-t border-t-black py-6 cursor-pointer"}>About</p>
-                    <p className={"border-t border-t-black py-6 cursor-pointer"}>Services</p>
-                    <p className={"border-t border-t-black py-6 cursor-pointer"}>Portfolio</p>
-                    <p className={"border-y border-y-black py-6 cursor-pointer"}>Contact</p>
+                    <div className={"flex flex-col md:mt-10 pl-10 pr-10 md:pl-10 md:pr-0 w-screen md:w-1/3"}>
+                        <Link href={"/"} className={"nav-link border-t border-t-black py-6 cursor-pointer"}>
+                            <span className="arrow">→</span> <span className="link-text">Home</span>
+                        </Link>
+                        <Link href={"/about"} className={"nav-link border-t border-t-black py-6 cursor-pointer"}>
+                            <span className="arrow">→</span> <span className="link-text">About</span>
+                        </Link>
+                        <Link href={"/services"} className={"nav-link border-t border-t-black py-6 cursor-pointer"}>
+                            <span className="arrow">→</span> <span className="link-text">Services</span>
+                        </Link>
+                        <Link href={"/portfolio"} className={"nav-link border-t border-t-black py-6 cursor-pointer"}>
+                            <span className="arrow">→</span> <span className="link-text">Portfolio</span>
+                        </Link>
+                        <Link href={"/contact"} className={"nav-link border-y border-y-black py-6 cursor-pointer"}>
+                            <span className="arrow">→</span> <span className="link-text">Contact</span>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>

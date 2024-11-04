68c9844e4c662bd7f31e269823b14c9867b93cd4
Ken <kel89@cornell.edu>
2024-11-04 16:30:20 -0700

    background image there but inverted

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,10 +2,22 @@ import Image from "next/image";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import ContactForm from "../components/contact-form";
+import ContactBackground from "../../public/images/group-4530.png";
 
 export default function Contact() {
     return (
-        <div className="relative h-screen w-screen bg-[#544F3D]">
+        <div className="relative h-screen w-screen bg-[#544F3D] bg-opacity-70">
+            <Image
+                src={ContactBackground}
+                fill
+                quality={100}
+                placeholder="blur"
+                sizes="100vw"
+                alt="Black Ink"
+                style={{
+                    objectFit: "cover",
+                }}
+            />
             <Header color="cream" />
             <div>
                 <div className="max-w-7xl mx-auto grid grid-cols-4 py-20 px-8 text-[#EFEEE8]">
@@ -19,15 +31,6 @@ export default function Contact() {
                 <div className="relative">
                     <Footer />
                 </div>
-            </div>
-            <div className="absolute inset-0">
-                <Image
-                    src="/group-4530.png"
-                    alt="Background Mask"
-                    layout="fill"
-                    objectFit="cover"
-                    style={{ mixBlendMode: "multiply" }}
-                />
             </div>
         </div>
     );

5157ef5cbf4c68f86dd5238b19375f5bad70be6d
Ken <kel89@cornell.edu>
2024-11-04 16:07:32 -0700

    tweaks and image

--- app/contact/page.js
+++ app/contact/page.js
@@ -5,7 +5,7 @@ import ContactForm from "../components/contact-form";
 
 export default function Contact() {
     return (
-        <div className="relatve h-screen w-screen bg-[#544F3D]">
+        <div className="relative h-screen w-screen bg-[#544F3D]">
             <Header color="cream" />
             <div>
                 <div className="max-w-7xl mx-auto grid grid-cols-4 py-20 px-8 text-[#EFEEE8]">
@@ -20,56 +20,15 @@ export default function Contact() {
                     <Footer />
                 </div>
             </div>
+            <div className="absolute inset-0">
+                <Image
+                    src="/group-4530.png"
+                    alt="Background Mask"
+                    layout="fill"
+                    objectFit="cover"
+                    style={{ mixBlendMode: "multiply" }}
+                />
+            </div>
         </div>
     );
-    // return (
-    //     <div className={"relative w-screen max-w-screen-2xl"}>
-    //         <Header color="cream" />
-    //         <div className={"text-[#EFEEE8] w-full"}>
-    //             <div
-    //                 className={
-    //                     "flex sticky top-0 justify-center w-screen h-screen bg-[#3A332E]"
-    //                 }
-    //             >
-    //                 <Image
-    //                     src="/images/about-bg.png"
-    //                     alt="Background Image"
-    //                     width={1920}
-    //                     height={1080}
-    //                     className={
-    //                         "mt-auto w-full min-h-[550px] object-cover object-[35%] mb-[25px] sm:mb-[150px]"
-    //                     }
-    //                     priority={true}
-    //                 />
-    //             </div>
-
-    //             <div
-    //                 className={
-    //                     "flex relative justify-center w-full min-h-screen bg-[#3A332E]"
-    //                 }
-    //             >
-    //                 {/* I think for contact this div needs its own background */}
-    //                 <div
-    //                     className={"flex border-t border-[#EFEEE8] mx-7 w-full"}
-    //                 >
-    //                     <div className={"flex my-10 w-full"}>
-    //                         <div
-    //                             className={
-    //                                 "flex flex-col-3 w-1/3 text-[18px] sm:text-[24px] font-ritma uppercase"
-    //                             }
-    //                         >
-    //                             <h1>Contact</h1>
-    //                         </div>
-    //                         <div className={"flex flex-col w-2/3"}>
-    //                             <ContactForm />
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //             <div className={"relative"}>
-    //                 <Footer />
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
 }

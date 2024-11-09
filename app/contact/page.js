222d0fcb55d7730fbebaa55e25606bf3058046d2
Ken <kel89@cornell.edu>
2024-11-09 08:43:55 -0700

    contact desktop backgorund mask working

--- app/contact/page.js
+++ app/contact/page.js
@@ -6,22 +6,22 @@ import ContactBackground from "../../public/images/group-4530.png";
 
 export default function Contact() {
     return (
-        <div className="relative h-screen w-screen bg-[#EFEEE8] lg:bg-[#544F3D] bg-opacity-70">
-            <Image
-                src={ContactBackground}
-                fill
-                quality={100}
-                placeholder="blur"
-                sizes="100vw"
-                alt="Black Ink"
-                className="-z-10"
-                style={{
-                    objectFit: "cover",
-                }}
-            />
+        <div className="relative h-screen w-screen">
             <Header color="cream" />
-            <div>
-                <div className="max-w-7xl h-[100dvh] mx-auto grid grid-cols-4 py-20 px-8 text-black lg:text-[#EFEEE8]">
+            <div className=" bg-[#EFEEE8] lg:bg-[#544F3D] bg-opacity-70 lg:bg-opacity-60">
+                <Image
+                    src={ContactBackground}
+                    fill
+                    quality={100}
+                    placeholder="blur"
+                    sizes="100vw"
+                    alt="Black Ink"
+                    className="-z-10"
+                    style={{
+                        objectFit: "cover",
+                    }}
+                />
+                <div className="max-w-7xl h-[100dvh] mx-auto grid grid-cols-4 py-32 px-8 text-black lg:text-[#EFEEE8]">
                     <h2 className="text-white font-ritma uppercase text-2xl mb-12 hidden lg:block">
                         Contact
                     </h2>

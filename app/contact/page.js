fdfc938c390606c8316df3c11ae84f696c01cf3d
Ken <kel89@cornell.edu>
2024-11-12 17:50:46 -0700

    background tweaked for contact still not happy with them

--- app/contact/page.js
+++ app/contact/page.js
@@ -8,30 +8,26 @@ export default function Contact() {
     return (
         <div className="relative h-screen w-screen">
             <Header color="cream" />
-            <div className=" bg-[#EFEEE8] lg:bg-[#544F3D] bg-opacity-70 lg:bg-opacity-60">
+            <div className="absolute inset-0 bg-[#EFEEE8] lg:bg-[#544F3D] -z-50">
                 <Image
                     src={ContactBackground}
-                    fill
                     quality={100}
                     placeholder="blur"
                     sizes="100vw"
                     alt="Black Ink"
-                    className="-z-10"
-                    style={{
-                        objectFit: "cover",
-                    }}
+                    className="w-full h-full object-cover opacity-30 lg:opacity-100"
                 />
-                <div className="max-w-7xl h-[100dvh] mx-auto grid grid-cols-4 py-32 px-8 text-black lg:text-[#EFEEE8]">
-                    <h2 className="text-white font-ritma uppercase text-2xl mb-12 hidden lg:block">
-                        Contact
-                    </h2>
-                    <div className="col-span-4 lg:col-span-3">
-                        <ContactForm />
-                    </div>
+            </div>
+            <div className="max-w-7xl h-[100dvh] mx-auto grid grid-cols-4 py-32 px-8 text-black lg:text-[#EFEEE8]">
+                <h2 className="text-white font-ritma uppercase text-2xl mb-12 hidden lg:block">
+                    Contact
+                </h2>
+                <div className="col-span-4 lg:col-span-3">
+                    <ContactForm />
                 </div>
-                <div className="relative text-white">
-                    <Footer />
-                </div>
+            </div>
+            <div className="relative text-white">
+                <Footer />
             </div>
         </div>
     );

8cdb00ed4d83724560aa8b927e9cacff003398ee
Tom Hall <thall1961@gmail.com>
2024-12-05 10:29:54 -0600

    i think its there

--- app/components/HeroAnimation.js
+++ app/components/HeroAnimation.js
@@ -1,69 +1,89 @@
 "use client";
 
-import animationData from '@/public/Ink_Intro.json';
-import "@/styles/intro-animation.css";
-import dynamic from 'next/dynamic';
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import React from "react";
 
-const Lottie = dynamic(() => import('react-lottie-player'), {ssr: false});
-
 export function HeroAnimation() {
-    const [playing, setPlaying] = React.useState(true);
-    const [animationPlayed, setAnimationPlayed] = React.useState(false);
-    
-    function onComplete() {
-        setPlaying(false);
-        sessionStorage.setItem('animationPlayed', 'true');
-        setTimeout(() => {
-            setAnimationPlayed(true);
-        }, 1000);
+  const [dotLottie, setDotLottie] = React.useState(null);
+  const [playing, setPlaying] = React.useState(true);
+  const [animationPlayed, setAnimationPlayed] = React.useState(false);
+
+  const dotLottieRefCallback = (dotLottie) => {
+    setDotLottie(dotLottie);
+  };
+
+  React.useEffect(() => {
+    const hasPlayed = sessionStorage.getItem("animationPlayed") === "true";
+    if (hasPlayed) {
+      setPlaying(false);
+      setAnimationPlayed(true);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    function isReady() {
+      dotLottie.setLayout({
+        ...dotLottie.layout,
+        fit: "cover",
+      });
     }
 
-    React.useEffect(() => {
-        const hasPlayed = sessionStorage.getItem('animationPlayed') === 'true';
-        if (hasPlayed) {
-            setPlaying(false);
-            setAnimationPlayed(true);
-        }
-    }, []);
+    function onPlay() {
+      setPlaying(true);
+    }
 
-    React.useEffect(() => {
-        // Add/remove no-scroll class on body when animation is playing
-        if (playing) {
-            document.body.style.overflow = "hidden";
-        } else {
-            document.body.style.overflow = "unset";
-        }
+    function onComplete() {
+      setPlaying(false);
+      sessionStorage.setItem("animationPlayed", "true");
+      setTimeout(() => {
+        setAnimationPlayed(true);
+      }, 1000);
+    }
 
-        // Cleanup when component unmounts
-        return () => {
-            document.body.style.overflow = "unset";
-        };
-    }, [playing]);
+    if (dotLottie) {
+      dotLottie.addEventListener("ready", isReady);
+      dotLottie.addEventListener("play", onPlay);
+      dotLottie.addEventListener("complete", onComplete);
+    }
 
-    return (
-        !animationPlayed ?
-            <div
-            id="hero-animation"
-                className={`z-[9999] relative transition-opacity duration-1000 ease-out ${
-                    playing && !animationPlayed ? 'opacity-100' : 'opacity-0 pointer-events-none'
-                }`}
-            >
-                <Lottie 
-                    play 
-                    loop={true} 
-                    animationData={animationData} 
-                    onComplete={onComplete}
-                    style={{
-                        position: 'fixed',
-                        top: 0,
-                        left: 0,
-                        right: 0
-                    }}
-                />
-            </div>
-        : null
-    );
+    return () => {
+      if (dotLottie) {
+        dotLottie.addEventListener("ready", isReady);
+        dotLottie.removeEventListener("play", onPlay);
+        dotLottie.removeEventListener("complete", onComplete);
+      }
+    };
+  }, [dotLottie, setPlaying]);
+
+  React.useEffect(() => {
+    // Add/remove no-scroll class on body when animation is playing
+    if (playing) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+
+    // Cleanup when component unmounts
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [playing]);
+
+  return !animationPlayed ? (
+    <div
+      className={`z-[9999] fixed top-0 left-0 right-0 transition-opacity duration-1000 ease-out ${
+        playing && !animationPlayed
+          ? "opacity-100"
+          : "opacity-0 pointer-events-none"
+      }`}
+    >
+      <DotLottieReact
+        src="/Ink_Intro.lottie"
+        loop={false}
+        autoplay={!animationPlayed}
+        dotLottieRefCallback={dotLottieRefCallback}
+        backgroundColor="#000000"
+      />
+    </div>
+  ) : null;
 }
-
-

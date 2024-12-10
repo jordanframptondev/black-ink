cf28669d865eeb6e3b4cb6e80988883743781b96
Tom Hall <thall1961@gmail.com>
2024-12-10 16:53:09 -0600

    mobile about

--- app/components/AboutSubHero.js
+++ app/components/AboutSubHero.js
@@ -3,8 +3,8 @@ import { PortableText } from "@portabletext/react";
 
 export function AboutSubHero({introQuote, introQuoteComponents}) {
   return (
-    <div id="sub-hero" className={"pt-[100px] sticky top-0 h-screen relative mt-[100vh]"}>
-        <div className={"flex justify-center min-w-screen h-screen bg-[#3A332E]"}>
+    <div id="sub-hero" className={"pt-[100px] sticky lg:top-0 min-h-screen relative mt-[100vh]"}>
+        <div className={"flex justify-center min-w-screen min-h-screen bg-[#3A332E]"}>
             <div className={"flex border-t border-[#EFEEE8] mx-10"}>
                 <div className={"flex flex-col md:flex-row my-10"}>
                     <div

200cd63a6280fc6481732ec4f5074afc07ca9088
jordan-frampton <jordan.frampton@flybreeze.com>
2024-12-10 11:58:07 -0700

    small update to the blog post body images

--- app/components/PostBody.js
+++ app/components/PostBody.js
@@ -5,20 +5,20 @@ import { urlFor } from "../utils/cms-service";
 
 const components = {
     block: {
-        h1: ({ children }) => <h1 className="text-4xl mb-4">{children}</h1>,
-        h2: ({ children }) => <h2 className="text-3xl mb-4">{children}</h2>,
-        h3: ({ children }) => (
+        h1: ({children}) => <h1 className="text-4xl mb-4">{children}</h1>,
+        h2: ({children}) => <h2 className="text-3xl mb-4">{children}</h2>,
+        h3: ({children}) => (
             <h3 className="text-2xl font-semibold mb-4">{children}</h3>
         ),
-        h4: ({ children }) => <h4 className="text-xl mb-4">{children}</h4>,
-        normal: ({ children }) => <p>{children}</p>,
+        h4: ({children}) => <h4 className="text-xl mb-4">{children}</h4>,
+        normal: ({children}) => <p>{children}</p>,
     },
     marks: {
-        strong: ({ children }) => (
+        strong: ({children}) => (
             <strong className="font-bold">{children}</strong>
         ),
-        em: ({ children }) => <em className="italic">{children}</em>,
-        link: ({ value, children }) => {
+        em: ({children}) => <em className="italic">{children}</em>,
+        link: ({value, children}) => {
             const target = value?.href?.startsWith("http")
                 ? "_blank"
                 : undefined;
@@ -35,35 +35,35 @@ const components = {
         },
     },
     list: {
-        bullet: ({ children }) => (
+        bullet: ({children}) => (
             <ul className="list-disc ml-5">{children}</ul>
         ),
-        number: ({ children }) => (
+        number: ({children}) => (
             <ol className="list-decimal ml-5">{children}</ol>
         ),
     },
-    listItem: ({ children }) => <li>{children}</li>,
+    listItem: ({children}) => <li>{children}</li>,
     types: {
-        image: ({ value }) => (
+        image: ({value}) => (
             <div className="my-6">
                 <Image
                     src={urlFor(value).url()}
                     alt={value.alt || "Image"}
                     width={800}
                     height={600}
-                    className="w-full h-auto object-contain"
+                    className="w-auto max-w-full h-auto object-contain"
                 />
             </div>
         ),
     },
 };
 
-export default function PostBody({ text }) {
+export default function PostBody({text}) {
     return (
         <FadeIn>
             <div className="post-body pt-16 md:pt-0">
                 <div className="mb-6 font-signifier text-xl md:text-2xl whitespace-pre-line">
-                    <PortableText value={text} components={components} />
+                    <PortableText value={text} components={components}/>
                 </div>
             </div>
         </FadeIn>
